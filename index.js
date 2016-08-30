e52f0e266668514610a79f24b6b7b38c0684bc11
Bernhard Matz <bernhard@trigo.at>
2016-08-30 17:45:28 +0200

    added ServicesList

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 const Service = require('./lib/Service');
-
+const servicesList = require('./lib/ServicesList');
 module.exports = {
-	Service: Service
+	Service: Service,
+	addService: servicesList.addService,
+	services: servicesList.services,
 };
