be9619e6c523027db63fb57ed09e1544e016679d
Christian Hubinger <christian@trigo.at>
2017-01-13 15:28:54 +0100

    Linting fixes

--- tests/end2end/services.js
+++ tests/end2end/services.js
@@ -1,11 +1,12 @@
 'use strict';
+
 const atrix = require('../..');
 const testServices = require('../../examples/');
 const supertest = require('supertest');
 
-var svcs = {};
-for (let i in atrix.services) {
-	let svc = atrix.services[i];
+const svcs = {};
+for (const i in atrix.services) {
+	const svc = atrix.services[i];
 	if (svc.config.config.endpoints.http) {
 		svcs[svc.name] = supertest(`http://localhost:${svc.config.config.endpoints.http.port}`);
 	}
