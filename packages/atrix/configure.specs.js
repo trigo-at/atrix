8e0638589361d564f73affcffc25c54a4280f139
Christian Hubinger <christian@trigo.at>
2017-01-23 15:20:05 +0100

    fix: configure.specs.js

--- packages/atrix/configure.specs.js
+++ packages/atrix/configure.specs.js
@@ -9,6 +9,10 @@ const { expect } = require('chai');
 
 describe('configure', () => {
 	it('merges configurations', () => {
+		Object.keys(cfgObj, key => {
+			delete cfgObj[key];
+		});
+
 		configure({ pluginSearchPaths: ['test'] });
 
 		expect(cfgObj.pluginSearchPaths).to.eql(['test']);
