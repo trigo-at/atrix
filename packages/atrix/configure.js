70fc9933a3f67fed28b6960a5fc745f7cd91848b
Christian Hubinger <christian@trigo.at>
2017-01-23 15:49:52 +0100

    fix: cleanup configure.js

--- packages/atrix/configure.js
+++ packages/atrix/configure.js
@@ -1,11 +1,8 @@
 'use strict';
 
-// let atrixConfig;
-
 module.exports = (configObj) => {
 	const atrixConfig = configObj;
 	return (cfg) => {
-		console.log('Merge config', cfg, ' into', atrixConfig);
 		Object.keys(cfg).forEach(key => {
 			atrixConfig[key] = cfg[key];
 		});
