7f71a88fab0de10f8ace427262dd76622e551d4a
Christian Hubinger <christian@trigo.at>
2018-04-16 17:42:24 +0200

    wip: basic tests for cors config

---
+++ tests/end2end/cors.specs.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const atrix = require('../..');
+const Chance = require('chance');
+const supertest = require('supertest');
+const { expect } = require('chai');
+
+const chance = new Chance();
+
+describe('CORS settings', () => {
+	let port, svc;
+
+	const startWithCorsCfg = async (cors) => {
+		port = chance.integer({ min: 20000, max: 30000 });
+		const service = new atrix.Service('cors', {
+			endpoints: {
+				http: {
+					port,
+					cors,
+				},
+			},
+		});
+
+		service.endpoints.add('http');
+
+		service.handlers.add('GET', '/', (req, reply) => {
+			return reply({ foo: 'bar' });
+		});
+
+		atrix.addService(service);
+		await atrix.services.cors.start();
+		svc = supertest(`http://localhost:${port}`);
+	};
+
+	afterEach(async () => {
+		await atrix.services.cors.stop();
+	});
+
+	it('setting "cors: true" makes server accept any origing (e.g. reflect orign header in "access-control-allow-origin")', async () => {
+		await startWithCorsCfg(true);
+
+		const res = await svc.options('/').set({
+			origin: 'http://wos-was-i.at',
+			'Access-Control-Request-Method': 'GET',
+		});
+		expect(res.headers['access-control-allow-origin']).to.eql('http://wos-was-i.at');
+	});
+
+	it('setting "cors: true" merges content-type,authorization & access-control-allow-origin into access-control-allow-headers', async () => {
+		await startWithCorsCfg(true);
+
+		const res = await svc.options('/').set({
+			origin: 'http://wos-was-i.at',
+			'Access-Control-Request-Method': 'GET',
+		});
+		expect(res.headers['access-control-allow-headers']).to.contain('content-type');
+		expect(res.headers['access-control-allow-headers']).to.contain('authorization');
+		expect(res.headers['access-control-allow-headers']).to.contain('access-control-allow-origin');
+	});
+});
