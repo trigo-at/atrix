bf0e50f5625ea4798c739047ac76ad0838e1a8c7
Christian Hubinger <christian@trigo.at>
2018-04-17 10:11:29 +0200

    fix: more cors tests & allow to overwrite array config values

--- tests/end2end/cors.specs.js
+++ tests/end2end/cors.specs.js
@@ -8,7 +8,8 @@ const { expect } = require('chai');
 const chance = new Chance();
 
 describe('CORS settings', () => {
-	let port, svc;
+	let port,
+		svc;
 
 	const startWithCorsCfg = async (cors) => {
 		port = chance.integer({ min: 20000, max: 30000 });
@@ -23,9 +24,7 @@ describe('CORS settings', () => {
 
 		service.endpoints.add('http');
 
-		service.handlers.add('GET', '/', (req, reply) => {
-			return reply({ foo: 'bar' });
-		});
+		service.handlers.add('GET', '/', (req, reply) => reply({ foo: 'bar' }));
 
 		atrix.addService(service);
 		await atrix.services.cors.start();
@@ -56,5 +55,35 @@ describe('CORS settings', () => {
 		expect(res.headers['access-control-allow-headers']).to.contain('content-type');
 		expect(res.headers['access-control-allow-headers']).to.contain('authorization');
 		expect(res.headers['access-control-allow-headers']).to.contain('access-control-allow-origin');
+	});
+
+	it('setting "cors: true" sets access-control-allow-credentials => true', async () => {
+		await startWithCorsCfg(true);
+
+		const res = await svc.options('/').set({
+			origin: 'http://wos-was-i.at',
+			'Access-Control-Request-Method': 'GET',
+		});
+		expect(res.headers['access-control-allow-credentials']).to.eql('true');
+	});
+
+	it('sets cors config object to priovided value', async () => {
+		const cors = {
+			origin: ['http://test.com'],
+			additionalHeaders: ['authorization', 'content-type'],
+			credentials: true,
+		};
+		await startWithCorsCfg(cors);
+		let res = await svc.options('/').set({
+			origin: 'http://test.com',
+			'Access-Control-Request-Method': 'GET',
+		});
+		expect(res.headers['access-control-allow-origin']).to.eql('http://test.com');
+		res = await svc.options('/').set({
+			origin: 'http://tasdest.com',
+			'Access-Control-Request-Method': 'GET',
+		});
+
+		expect(res.headers['access-control-allow-origin']).not.to.exist;
 	});
 });
