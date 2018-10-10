c582722687e3086fbc50109abae33c17f989a817
Christian Hubinger <christian@trigo.at>
2018-10-10 10:21:03 +0200

    fix: remove default config

--- examples/load-from-dir-svc/config.js
+++ examples/load-from-dir-svc/config.js
@@ -5,12 +5,6 @@ module.exports = {
 		http: {
 			port: 3007,
 			handlerDir: `${__dirname}/handlers`,
-			cors: true,
-			requestLogger: {
-				enabled: false,
-				logFullRequest: true,
-				logFullResponse: true,
-			},
 		},
 	},
 };
