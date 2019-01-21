9ea00f5768d53d375c395c32e5e3476fd5ae6338
Christian Hubinger <christian@trigo.at>
2019-01-21 10:56:59 +0100

    feature:  make use of hapi-auth-signedlink "failAction" configuration

---
+++ tests/end2end/signedlink-secured-svc.specs.js
@@ -0,0 +1,85 @@
+'use strict';
+
+/* eslint-env node, mocha */
+/* eslint prefer-arrow-callback: 0, func-names: 0, space-before-function-paren: 0 */
+
+const atrix = require('../..');
+const Chance = require('chance');
+const supertest = require('supertest');
+const expect = require('chai').expect;
+
+const chance = new Chance();
+
+describe('signedlink-secured-svc', () => {
+    let svc;
+    let service;
+    const startService = async (
+        options = {
+            secret: 'test-secret',
+        }
+    ) => {
+        const port = chance.integer({min: 20000, max: 30000});
+        service = atrix.addService({
+            name: 'signedlinkSecured',
+            endpoints: {
+                http: {
+                    port,
+                    cors: true,
+                },
+            },
+            security: {
+                strategies: {
+                    signedlink: options,
+                },
+                endpoints: {
+                    signedlink: ['/signedlink.*'],
+                },
+            },
+        });
+        service.handlers.add('GET', '/signedlink', (req, reply) => reply({foo: 'bar'}));
+
+        service.handlers.add('GET', '/test', (req, reply) => reply({foo: 'bar'}));
+
+        await service.start();
+        svc = supertest(`http://localhost:${port}`);
+    };
+
+    afterEach(async () => {
+        await service.stop();
+    });
+
+    it('GET /signedlink is secured', async () => {
+        await startService();
+        const res = await svc.get('/signedlink');
+        expect(res.statusCode).to.equal(401);
+    });
+
+    it('GET /signedlink is secured using "failAction" on error', async () => {
+        let reas;
+        await startService({
+            secret: 'test-secret',
+            failAction: async (request, h, reason) => {
+                reas = reason;
+                return h.redirect('http://www.google.com').takeover();
+            },
+        });
+
+        const res = await svc.get('/signedlink');
+        expect(res.statusCode).to.equal(302);
+        expect(res.headers.location).to.equal('http://www.google.com');
+        expect(reas).to.eql('param "auth" missing');
+    });
+
+    it('GET /test is not secured', async () => {
+        await startService();
+        const res = await svc.get('/test');
+        expect(res.statusCode).to.equal(200);
+    });
+
+    it('"signedlink" strategy add "createSignedLink" utility to the service', async () => {
+        await startService();
+        const link = atrix.services.signedlinkSecured.createSignedLink('/signedlink');
+        const res = await svc.get(link);
+        expect(res.statusCode).to.equal(200);
+    });
+});
